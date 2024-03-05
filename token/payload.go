1c05e8253886cdbaa3c572d88aa72c0db3818344
Xinnan.Alex <leongxinnan@outlook.my>
2024-03-06 00:05:32 +0800

    implemented jwttokenmaker, pasetomaker

---
+++ token/payload.go
@@ -0,0 +1,44 @@
+package token
+
+import (
+	"errors"
+	"github.com/google/uuid"
+	"time"
+)
+
+var (
+	ErrExpiredToken = errors.New("token has expired")
+	ErrInvalidToken = errors.New("token is invalid")
+)
+
+// Payload contains the payload data of the token
+type Payload struct {
+	ID        uuid.UUID
+	Username  string
+	IssuedAt  time.Time
+	ExpiredAt time.Time
+}
+
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        tokenID,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+
+	return payload, nil
+}
+
+// Valid checks if the token payload is valid or not
+func (payload *Payload) Valid() error {
+	if time.Now().After(payload.ExpiredAt) {
+		return ErrExpiredToken
+	}
+	return nil
+}
